81faa68c2561a63d76bc0c009c2b2f8781f25c23
developershubham360-bit <developer.shubham360@gmail.com>
2026-08-13 22:17:36 +0530

    Add: minimum-stack - submission-0

---
+++ Data Structures & Algorithms/minimum-stack/submission-0.cpp
@@ -0,0 +1,25 @@
+class MinStack {
+private: 
+    stack<int> stack, minStack;
+public:
+    MinStack() {}
+    
+    void push(int val) {
+        stack.push(val);
+        val = min(val, minStack.empty()?val:minStack.top());
+        minStack.push(val);
+    }
+    
+    void pop() {
+        stack.pop();
+        minStack.pop();
+    }
+    
+    int top() {
+        return stack.top();
+    }
+    
+    int getMin() {
+        return minStack.top();
+    }
+};
